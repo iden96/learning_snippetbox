72024c6b246ad2ec605a5273fef2c9a0dc1632ac
Denis Krugly <krugly_d@coinmatics.io>
2022-09-27 16:43:24 +0300

    [feature] add different kinds of tests

---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,37 @@
+package mysql
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	dsn := os.Getenv("TEST_MYSQL_DSN")
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := os.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return db, func() {
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	}
+}
